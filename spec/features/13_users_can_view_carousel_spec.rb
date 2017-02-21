2db164857f74119bd7803dfc7b8c3a9c7bfd7fe6
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-21 18:42:17 -0500

    adding carousel test

--- spec/features/13_users_can_view_carousel_spec.rb
+++ spec/features/13_users_can_view_carousel_spec.rb
@@ -1,28 +1,20 @@
 require "rails_helper"
 
 feature "user visits home page" do
+  let!(:blue_dragon) { FactoryGirl.create(:restaurant) }
+  let!(:o_ya) { FactoryGirl.create(:restaurant, name: "O Ya") }
 
-  scenario "user sees carousel page" do
+  scenario "signed-in user sees carousel page" do
 
     visit "/"
 
+    expect(page).to have_content "All the information you need to make the best
+    decisions possible for Boston's restaurant week: Dine Out Boston."
+
+    first(:button, "Enter").click
+
+    expect(page).to have_content "Participating restaurants"
     expect(page).to have_content "Blue Dragon"
-    expect(page).to have_content "Boston - Fort Point"
-    expect(page).to have_content "324 A Street Boston, MA 02210"
-    expect(page).to have_content "Asian"
-    expect(page).to have_content "$20"
-    expect(page).to have_content "$38"
-    expect(page).to have_content "Open daily"
     expect(page).to have_content "O Ya"
-  end
-
-  scenario "restaurants are listed in alphabetical order" do
-
-    visit "/restaurants"
-
-    expect(page).to have_selector("ul.restaurant-info li:nth-child(1)",
-      visible: blue_ginger.name)
-    expect(page).to have_selector("ul.restaurant-info li:nth-child(2)",
-      visible: o_ya.name)
   end
 end
