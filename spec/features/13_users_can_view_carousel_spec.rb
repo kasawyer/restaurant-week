b5588a40dad117c11090d2f5bd604c70aaefc42a
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-21 18:46:13 -0500

    updating carousel test

--- spec/features/13_users_can_view_carousel_spec.rb
+++ spec/features/13_users_can_view_carousel_spec.rb
@@ -17,4 +17,27 @@ feature "user visits home page" do
     expect(page).to have_content "Blue Dragon"
     expect(page).to have_content "O Ya"
   end
+
+  scenario "non-signed-in user sees carousel page" do
+
+    visit "/"
+
+    expect(page).to have_content "All the information you need to make the best
+    decisions possible for Boston's restaurant week: Dine Out Boston."
+    expect(page).to have_content "Log in to add reviews and save favorites, or
+    continue to view restaurants."
+
+    first(:button, "Enter").click
+
+    expect(page).to have_content "Participating restaurants"
+    expect(page).to have_content "Blue Dragon"
+    expect(page).to have_content "O Ya"
+
+    visit "/"
+    first(:button, "Log in").click
+
+    expect(page).to have_content "Log in"
+    expect(page).to have_content "Email"
+    expect(page).to have_content "Password"
+  end
 end
